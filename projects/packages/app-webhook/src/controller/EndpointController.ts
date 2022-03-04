b3dd54586ff0304686d7c44e9c3c987db3374d9a
Jibao Mansaray <jibaomansaray@gmail.com>
2022-03-04 16:53:44 +0000

    Make sure the topic belongs to an application

--- projects/packages/app-webhook/src/controller/EndpointController.ts
+++ projects/packages/app-webhook/src/controller/EndpointController.ts
@@ -1,16 +1,23 @@
 
 import { NextFunction, Request, Response } from "express";
-import { endpointRepo } from '../repos'
+import { WebhookTopic } from "../entity/WebhookTopic";
+import { endpointRepo, topicRepo } from '../repos'
+import { ApiControllerHelper } from "./ApiControllerHelper";
 
-export class DeliveryController {
+export class EndpointController {
 
   private repo = endpointRepo();
 
   async all(request: Request, response: Response, next: NextFunction) {
-    return this.repo.find();
+    const app = await ApiControllerHelper.fetchApp(request, response);
+    if (app) {
+      return this.repo.find();
+    }
   }
 
   async one(request: Request, response: Response, next: NextFunction) {
+    const app = await ApiControllerHelper.fetchApp(request, response);
+
     const record = await this.repo.findOne(request.params.id);
 
     if (record) {
@@ -22,21 +29,65 @@ export class DeliveryController {
   }
 
   async save(request: Request, response: Response, next: NextFunction) {
-    return this.repo.save(request.body);
+    const app = await ApiControllerHelper.fetchApp(request, response);
+    const topicIdOrName = request.body.topic;
+    let topic: WebhookTopic;
+
+    if (!app) {
+      return;
+    }
+
+    if (!isNaN(topicIdOrName)) {
+      topic = await topicRepo().findOneByIdAndApp(topicIdOrName, app);
+    } else {
+      topic = await topicRepo().findOneByNameAndApp(topicIdOrName, app);
+    }
+
+    if (topic) {
+      request.body.isActive = true;
+      request.body.app = app.id;
+      request.body.topic = topic.id;
+
+      try {
+        return await this.repo.save(request.body);
+      } catch (e) {
+        response.status(400);
+        response.json({ error: e.message })
+      }
+    } 
+
+    response.status(404);
+    response.json({ error: 'topic does not exist' })
   }
 
   async update(request: Request, response: Response, next: NextFunction) {
-    const record = await this.repo.findOne(request.params.id);
-    if (record) {
-      return await this.repo.update(record.id, request.body);
-    } else {
-      response.status(404);
-      response.json({ error: 'not found' });
-    }
+    response.status(400);
+    response.json({ error: 'you are not allowed to update a registered endpoint' });
   }
 
   async remove(request: Request, response: Response, next: NextFunction) {
+    const app = await ApiControllerHelper.fetchApp(request, response);
+    const topicIdOrName = request.params.topic;
+    let topic: WebhookTopic;
+
+    if (!app) {
+      return;
+    }
+
+    if (!isNaN(topicIdOrName)) {
+      topic = await topicRepo().findOneByIdAndApp(topicIdOrName, app);
+    } else {
+      topic = await topicRepo().findOneByNameAndApp(topicIdOrName, app);
+    }
+
     const record = await this.repo.findOne(request.params.id);
-    return await this.repo.softRemove(record);
+
+    if (record.app === app.id && record.topic === topic.id) {
+      return await this.repo.softRemove(record);
+    }
+
+    response.status(400);
+    response.json({ error: 'app or topic is wrong' });
+
   }
 }
