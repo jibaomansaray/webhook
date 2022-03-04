5c1ea9810c23725937b69a8bcefe309789546065
Jibao Mansaray <jibaomansaray@gmail.com>
2022-03-04 06:44:47 +0000

    Make sure the topic is attached to an application

--- projects/packages/app-webhook/src/controller/TopicController.ts
+++ projects/packages/app-webhook/src/controller/TopicController.ts
@@ -1,42 +1,73 @@
 
 import { NextFunction, Request, Response } from "express";
 import { topicRepo } from '../repos'
+import { ApiControllerHelper } from "./ApiControllerHelper";
 
 export class TopicController {
 
   private repo = topicRepo();
 
   async all(request: Request, response: Response, next: NextFunction) {
-    return this.repo.find();
+    const app = await ApiControllerHelper.fetchApp(request, response);
+    if (app) {
+      return this.repo.findByApp(app);
+    }
   }
 
   async one(request: Request, response: Response, next: NextFunction) {
-    const record = await this.repo.findOne(request.params.id);
+    const app = await ApiControllerHelper.fetchApp(request, response);
 
-    if (record) {
-      return record
-    } else {
-      response.status(404);
-      response.json({ error: 'not found' })
+    if (app) {
+      const record = await this.repo.findOneByIdAndApp(request.params.id, app);
+
+      if (record) {
+        return record;
+      } else {
+        response.status(404);
+        response.json({ error: 'not found' })
+      }
     }
   }
 
   async save(request: Request, response: Response, next: NextFunction) {
-    return this.repo.save(request.body);
+    const app = await ApiControllerHelper.fetchApp(request, response);
+    if (app) {
+      request.body.app = app.id
+      const existing = await this.repo.findOneByNameAndApp(request.body.name, app);
+      if (existing) {
+        response.status(404);
+        response.json({ error: 'a topic with this name already exist' });
+        return;
+      }
+
+      return this.repo.save(request.body);
+    }
   }
 
   async update(request: Request, response: Response, next: NextFunction) {
-    const record = await this.repo.findOne(request.params.id);
-    if (record) {
-      return await this.repo.update(record.id, request.body);
-    } else {
-      response.status(404);
-      response.json({ error: 'not found' });
+    const app = await ApiControllerHelper.fetchApp(request, response);
+    if (app) {
+      const record = await this.repo.findOneByIdAndApp(request.params.id, app);
+      if (record) {
+        request.body.name = record.name;
+        return await this.repo.update(record.id, request.body);
+      } else {
+        response.status(404);
+        response.json({ error: 'not found' });
+      }
     }
   }
 
   async remove(request: Request, response: Response, next: NextFunction) {
-    const record = await this.repo.findOne(request.params.id);
-    return await this.repo.softRemove(record);
+    const app = await ApiControllerHelper.fetchApp(request, response);
+    if (app) {
+      const record = await this.repo.findOneByIdAndApp(request.params.id, app);
+      if (record) {
+        return await this.repo.softRemove(record);
+      }
+
+      response.status(404);
+      response.json({ error: 'not found' });
+    }
   }
 }
