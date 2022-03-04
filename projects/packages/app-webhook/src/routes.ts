4a50356581e93ea58ee4e3a07841a0c918f39869
Jibao Mansaray <jibaomansaray@gmail.com>
2022-03-04 06:49:17 +0000

    Include the "app" as a requirement for most routes

--- projects/packages/app-webhook/src/routes.ts
+++ projects/packages/app-webhook/src/routes.ts
@@ -3,47 +3,64 @@ import { AppController } from "./controller/AppController";
 import { DeliveryController } from "./controller/DeliveryController";
 import { EventController } from './controller/EventController';
 import { TopicController } from './controller/TopicController';
+import { DispatchController } from './controller/DispatchController';
 
-const generateRoutes = (endpoint: string, controller: unknown) => {
+const generateRoutes = (endpoint: string, controller: unknown, viaApp: boolean = false) => {
 
     return [
         {
             method: "get",
-            route: `/${endpoint}`,
+            route: (viaApp)? `/${endpoint}/:app`: `/${endpoint}`,
             controller,
-            action: "all"
+            action: "all",
+            name: 'get_all'
         },
         {
             method: "get",
-            route: `/${endpoint}/:id`,
+            route: (viaApp)? `/${endpoint}/:app/:id`: `/${endpoint}/:id`,
             controller,
-            action: "one"
+            action: "one",
+            name: 'get_one'
         },
         {
             method: "post",
-            route: `/${endpoint}`,
+            route: (viaApp)? `/${endpoint}/:app`: `/${endpoint}`,
             controller,
-            action: "save"
+            action: "save",
+            name: 'save_one'
         },
         {
             method: "put",
-            route: `/${endpoint}/:id`,
+            route: (viaApp)? `/${endpoint}/:app/:id`: `/${endpoint}/:id`,
             controller,
-            action: "update"
+            action: "update",
+            name: 'update_one'
         },
         {
             method: "delete",
-            route: `/${endpoint}/:id`,
+            route: (viaApp) ? `/${endpoint}/:app/:id`: `/${endpoint}/:id`,
             controller,
-            action: "remove"
+            action: "remove",
+            name: 'delete_one'
         }
     ];
 }
 
+
+const dispatchRoutes = [
+    {
+        method: "post",
+        route: '/api/dispatches/:app',
+        controller: DispatchController,
+        action: "save"
+    },
+];
+
 export const Routes = [
     ...generateRoutes('api/apps', AppController),
-    ...generateRoutes('api/deliveries', DeliveryController),
-    ...generateRoutes('api/endpoints', EntityRepository),
-    ...generateRoutes('api/events', EventController),
-    ...generateRoutes('api/topics', TopicController)
+    ...generateRoutes('api/deliveries', DeliveryController, true),
+    ...generateRoutes('api/endpoints', EntityRepository, true),
+    ...generateRoutes('api/events', EventController, true),
+    ...generateRoutes('api/topics', TopicController, true),
+    ...dispatchRoutes,
 ]
