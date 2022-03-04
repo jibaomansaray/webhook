af4cf44230f6eb4f03fc64ac7ad1714345ef6990
Jibao Mansaray <jibaomansaray@gmail.com>
2022-03-04 16:50:58 +0000

    Remove the stream queries.
    I could not get them working.

--- projects/packages/app-webhook/src/backgroud/index.ts
+++ projects/packages/app-webhook/src/backgroud/index.ts
@@ -1,41 +1,53 @@
 import { createConnection } from "typeorm";
 import { eventRepo, deliveryRepo, endpointRepo, topicRepo, appRepo } from '../repos'
 import { dispatchQueue, broadcastQueue } from '../queue'
+import { DeliveryStatus } from "../types/deliverystatus";
+import { deliver } from "./deliver";
 
-
-// register queue processor
-dispatchQueue.process((job, done) => {
-  createConnection().then(async (connection) => {
-    console.log('reading.....')
-    console.table(job.data);
+createConnection().then(async (connection) => {
+  // register queue processor
+  dispatchQueue.process(async (job, done) => {
     const data = job.data as { eventId: number, topicId: number, appId: number };
     const event = await eventRepo().findOne(data.eventId);
     const topic = await topicRepo().findOne(data.topicId);
     const app = await appRepo().findOne(data.appId);
 
-    connection.close();
-    done()
-  }).catch(() => {
-    done();
+    const stream = await endpointRepo().findByAppAndTopicStream(app, topic, { isActive: true });
+
+    stream.on('end', () => {
+      stream.destroy()
+      done()
+    });
+
+    stream.on('error', (e) => {
+      console.log('error', e);
+    })
+
+    stream.on('data', async (data) => {
+      const record = JSON.parse(JSON.stringify(data)) as { WebhookEndpoint_id: number }
+
+      await deliveryRepo().save({
+        endpoint: record.WebhookEndpoint_id,
+        event: event.id,
+        status: DeliveryStatus.PENDING,
+        nextAttempt: new Date()
+      });
+    })
   });
-});
 
-
-// broadcast event to external systems
-broadcastQueue.process((job, done) => { 
-  createConnection().then(async (connection) => {
-    console.log('try broadcasting....: 1' + Date.now())
-    connection.close();
-
-    // plock the next batch
-    done();
-    broadcastQueue.add({});
-  }).catch(() => {
+  // broadcast event to external systems
+  broadcastQueue.process(async (_job, done) => {
+    await deliver()
     done()
-  })
-});
+    broadcastQueue.add({});
+  });
+
+  // start broadcasting
+  broadcastQueue.add({});
+  // broadcastQueue.add({}, { repeat: { cron: '* * * * *' } });
+
+})
 
 
-// start broad casting
-broadcastQueue.add({});
+
 
