f4c3d6f085732199def25a46c2bbf3f962e7b292
Jibao Mansaray <jibaomansaray@gmail.com>
2022-03-04 16:58:57 +0000

    Add new queries that fetches a limited number of records

--- projects/packages/app-webhook/src/repository/WebhookDeliveryRespository.ts
+++ projects/packages/app-webhook/src/repository/WebhookDeliveryRespository.ts
@@ -1,7 +1,32 @@
-import {EntityRepository, Repository} from "typeorm";
+import { EntityRepository, Repository } from "typeorm";
 import { WebhookDelivery } from '../entity/WebhookDelivery';
+import { DeliveryStatus } from "../types/deliverystatus";
 
 @EntityRepository(WebhookDelivery)
 export class WebhookDeliveryRepository extends Repository<WebhookDelivery> {
+
+
+  public async findByStatus(status: DeliveryStatus, limit = 200, where: { [key: string]: unknown } = {}): Promise<WebhookDelivery[]> {
+    return this.createQueryBuilder()
+      .leftJoinAndSelect("WebhookDelivery.endpoint", "WebhookEndpoint")
+      .leftJoinAndSelect("WebhookDelivery.event", "WebhookEvent")
+      .where({
+        status,
+        ...where
+      })
+      .andWhere("WebhookEndpoint.isActive = :isActive", { isActive: true })
+      .limit(limit || 200)
+      .getMany()
+  }
+
+  public async findByStatusAndNextAttempt(status: DeliveryStatus, next: Date, limit = 200): Promise<WebhookDelivery[]> {
+    return this.createQueryBuilder()
+      .leftJoinAndSelect("WebhookDelivery.endpoint", "WebhookEndpoint")
+      .leftJoinAndSelect("WebhookDelivery.event", "WebhookEvent")
+      .where(`status = :status and nextAttempt <= :next`, { status, next })
+      .andWhere("WebhookEndpoint.isActive = :isActive", { isActive: true })
+      .limit(limit || 200)
+      .getMany()
+  }
 
 }
