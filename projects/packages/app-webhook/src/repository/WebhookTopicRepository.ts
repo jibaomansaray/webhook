21a3679ceffe12ab3b3641dd2c209a4c687f243d
Jibao Mansaray <jibaomansaray@gmail.com>
2022-03-04 06:48:40 +0000

    Add custom methods to the topic repository

--- projects/packages/app-webhook/src/repository/WebhookTopicRepository.ts
+++ projects/packages/app-webhook/src/repository/WebhookTopicRepository.ts
@@ -1,7 +1,31 @@
 import {EntityRepository, Repository} from "typeorm";
+import { WebhookApp } from "../entity/WebhookApp";
 import { WebhookTopic } from '../entity/WebhookTopic';
 
 @EntityRepository(WebhookTopic)
 export class WebhookTopicRepository extends Repository<WebhookTopic> {
   
+  public async findOneByName(name: string): Promise<WebhookTopic | undefined> {
+    return this.findOne({ name });
+  }
+
+  public async findOneByIdAndApp(id: number, app: WebhookApp): Promise<WebhookTopic | undefined > {
+    return this.findOne({
+      id,
+      app: app.id
+    });
+  }
+
+  public async findOneByNameAndApp(name: string, app: WebhookApp): Promise<WebhookTopic | undefined > {
+    return this.findOne({
+      name,
+      app: app.id
+    });
+  }
+
+  public async findByApp(app: WebhookApp): Promise<WebhookTopic[]> {
+    return this.find({
+      app: app.id
+    });
+  }
 }
